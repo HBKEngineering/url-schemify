39674140ec94956fcb0dc677a0376f250b0d0c27
Brett Neese <brneese@brneese.com>
2016-04-28 13:49:21 -0500

    Allow configuring via an env var

--- url-schemify.js
+++ url-schemify.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var URL_SCHEME_REGEXP = /^((?:f|ht)tps?:)?\/\//;
-var DEFAULT_SCHEME = 'https';
+var DEFAULT_SCHEME = process.env.SCHEME || 'https';
 
 module.exports = function(url, options) {
   options || (options = {});
